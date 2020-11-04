946b6dac4fd3dceb5456f95984f1afb0f0fcae3e
ariel-simnegar <ariel.simnegar@gmail.com>
2020-11-04 11:58:11 -0500

    Add login and registration API

---
+++ client/login.js
@@ -0,0 +1,73 @@
+// localStorage keys
+const USERNAME_STORAGE_KEY = 'spendify_username';
+const PASSWORD_STORAGE_KEY = 'spendify_password';
+const REMEMBER_STORAGE_KEY = 'spendify_remember';
+
+async function login(username, password) {
+    const response = await fetch('/loginUser', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({ username, password })
+    });
+    return await response.json();
+}
+
+window.addEventListener('load', async () => {
+    const storage = window.localStorage;
+
+    const loginFormEl = document.getElementById('login-form');
+    const usernameEl = document.getElementById('username');
+    const passwordEl = document.getElementById('password');
+    const rememberEl = document.getElementById('remember');
+
+    // Restore saved username, password, and remember me checkmark
+    const savedUsername = storage.getItem(USERNAME_STORAGE_KEY);
+    const savedPassword = storage.getItem(PASSWORD_STORAGE_KEY);
+    const savedRemember = storage.getItem(REMEMBER_STORAGE_KEY);
+    if (savedUsername !== null) {
+        usernameEl.value = savedUsername;
+    }
+    if (savedPassword !== null) {
+        passwordEl.value = savedPassword;
+    }
+    if (savedRemember !== null) {
+        rememberEl.checked = Boolean(savedRemember);
+    }
+
+    function rememberMe() {
+        storage.setItem(REMEMBER_STORAGE_KEY, rememberEl.checked);
+        if (rememberEl.checked) {
+            storage.setItem(USERNAME_STORAGE_KEY, usernameEl.value);
+            storage.setItem(PASSWORD_STORAGE_KEY, passwordEl.value);
+        } else {
+            storage.removeItem(USERNAME_STORAGE_KEY);
+            storage.removeItem(PASSWORD_STORAGE_KEY);
+        }
+    }
+
+    document.getElementById('login').addEventListener('click', async () => {
+        loginFormEl.classList.add('was-validated');
+        if (loginFormEl.checkValidity()) {
+            const feedback = await login(usernameEl.value, passwordEl.value);
+            if (feedback.error) {
+                alert(feedback.message);
+            } else {
+                console.log(feedback);
+                rememberMe();
+                window.location.href = 'dashboard.html';
+            }
+        }
+    });
+
+    document.getElementById('register').addEventListener('click', () => {
+        loginFormEl.classList.add('was-validated');
+        if (loginFormEl.checkValidity()) {
+            rememberMe();
+            storage.setItem('register_spendify_username', usernameEl.value);
+            storage.setItem('register_spendify_password', passwordEl.value);
+            window.location.href = 'register.html';
+        }
+    });
+});
---
+++ client/register.js
@@ -0,0 +1,66 @@
+
+const REGISTER_USERNAME_STORAGE_KEY = 'register_spendify_username';
+const REGISTER_PASSWORD_STORAGE_KEY = 'register_spendify_password';
+
+window.addEventListener('load', async () => {
+    const storage = window.localStorage;
+    const savedUsername = storage.getItem(REGISTER_USERNAME_STORAGE_KEY);
+    const savedPassword = storage.getItem(REGISTER_PASSWORD_STORAGE_KEY);
+    storage.removeItem(REGISTER_USERNAME_STORAGE_KEY);
+    storage.removeItem(REGISTER_PASSWORD_STORAGE_KEY);
+
+    const registrationFormEl = document.getElementById('registration-form');
+    const realnameEl = document.getElementById('realname');
+    const usernameEl = document.getElementById('username');
+    const passwordEl = document.getElementById('password');
+    const bankUsernameEl = document.getElementById('bank-username');
+    const bankPasswordEl = document.getElementById('bank-password');
+    const accountNumberEl = document.getElementById('account-number');
+    const routingNumberEl = document.getElementById('routing-number');
+    const addressEl = document.getElementById('address');
+    const cityEl = document.getElementById('city');
+    const stateEl = document.getElementById('state');
+    const zipEl = document.getElementById('zip');
+
+    if (savedUsername !== null) {
+        usernameEl.value = savedUsername;
+    }
+    if (savedPassword !== null) {
+        passwordEl.value = savedPassword;
+    }
+
+        console.log(registrationFormEl.checkValidity());
+    document.getElementById('register').addEventListener('click', async () => {
+        registrationFormEl.classList.add('was-validated');
+        if (registrationFormEl.checkValidity()) {
+            const response = await fetch('/registerUser', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify({
+                    realname: realnameEl.value,
+                    username: usernameEl.value,
+                    password: passwordEl.value,
+                    bankUsername: bankUsernameEl.value,
+                    bankPassword: bankPasswordEl.value,
+                    accountNumber: accountNumberEl.value,
+                    routingNumberEl: routingNumberEl.value,
+                    address: addressEl.value,
+                    city: cityEl.value,
+                    state: stateEl.value,
+                    zip: zipEl.value
+                })
+            });
+            const feedback = await response.json();
+            if (feedback.error) {
+                alert(feedback.message);
+            } else {
+                console.log(feedback);
+                window.location.href = window.location.href.substring(
+                    0, window.location.href.lastIndexOf('/')
+                ) + '/dashboard.html';
+            }
+        }
+    });
+});
